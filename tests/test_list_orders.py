c223c4de02cf7686a31ff020672d4f6723729d75
vvilin98 <vvilin98@yandex.ru>
2024-03-16 22:00:08 +0300

    fix test_list_orders.py

--- tests/test_list_orders.py
+++ tests/test_list_orders.py
@@ -8,4 +8,4 @@ class TestReturnOrderList:
     @allure.title('В тело ответа возвращается список заказов')
     def test_list_order(self):
         response = requests.get(f'{Urls.URL}{Handle.CREATE_ORDER}')
-        assert len(response.json()) > 0
+        assert response.status_code == 200 and "orders" in response.json()
